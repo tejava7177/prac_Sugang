840ebbe7b9da3791de37cc976dfc3de947b96d6f
Sim-JuHeun <juheun9912@gmail.com>
2025-05-31 13:24:08 +0900

    기본설정

---
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/SugangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtone.sugang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugangBackendApplication.class, args);
+    }
+
+}
---
+++ sugang_backend/src/test/java/com/cloudtone/sugang_backend/SugangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtone.sugang_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
