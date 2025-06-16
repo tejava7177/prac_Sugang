34734d1dbea729fc0fff39dcaf149461ef53607d
Sim-JuHeun <juheun9912@gmail.com>
2025-06-16 16:45:27 +0900

    테스트 주석

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/SugangBackendApplication.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/SugangBackendApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SugangBackendApplication {
-
+        //테스트 주석
     public static void main(String[] args) {
         SpringApplication.run(SugangBackendApplication.class, args);
     }
-
 }
