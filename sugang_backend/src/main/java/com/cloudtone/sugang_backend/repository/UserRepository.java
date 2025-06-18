fa89b18c022b2703847a3fc0c5c4f621e62ab227
SungYeon <yousung1018@naver.com>
2025-06-18 23:36:06 +0900

    User setting

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/repository/UserRepository.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.cloudtone.sugang_backend.repository;
 
-public interface UserRepository {
+import com.cloudtone.sugang_backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 학번으로 사용자 조회
+    Optional<User> findByStudentNumber(String studentNumber);
 }
--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/service/UserService.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/service/UserService.java
@@ -1,17 +1,29 @@
 package com.cloudtone.sugang_backend.service;
 
 import com.cloudtone.sugang_backend.domain.User;
+import com.cloudtone.sugang_backend.dto.AddUserRequest;
 import com.cloudtone.sugang_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // TODO: UserRepository, save 메서드 등 구현 필요
-    //메서드 추가
-    public User findById(Long userId){
-        return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .studentNumber(dto.getStudentNumber())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .name(dto.getName())
+                .build()).getId();
     }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("옳지 않은 사용자입니다."));
+    }
+
 }
