1667bf6c1ba562d2b16380d233338f8445f3ee24
haeuniiii <whgkdms020506@gmail.com>
2025-06-19 23:58:39 +0900

    userservice 작성

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/Service/UserService.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/Service/UserService.java
@@ -1,4 +1,17 @@
 package com.cloudtone.sugang_backend.Service;
 
+import com.cloudtone.sugang_backend.domain.User;
+import com.cloudtone.sugang_backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    // TODO: UserRepository, save 메서드 등 구현 필요
+    //메서드 추가
+    public User findById(Long userId){
+        return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
 }
