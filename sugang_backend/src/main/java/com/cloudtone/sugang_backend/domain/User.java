2c9351bcbfe9456bf8a1320abf08aaef9f63f47b
haeuniiii <whgkdms020506@gmail.com>
2025-06-19 22:27:27 +0900

    user 가져옴

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/domain/User.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/domain/User.java
@@ -1,4 +1,82 @@
 package com.cloudtone.sugang_backend.domain;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "studentNumber", nullable = false, unique = true, length = 8)
+    private String studentNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Builder
+    public User(String studentNumber, String password, String name) {
+        this.studentNumber = studentNumber;
+        this.password = password;
+        this.name = name;
+    }
+
+    @Override // 권한 변경
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    // 사용자의 id를 반환 (고유한 값)
+    @Override
+    public String getUsername() {
+        return studentNumber;
+    }
+
+    // 사용자의 패스워드 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않았음.
+    }
+
+    // 계정 잠금여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // 잠금 아님 Non
+    }
+
+    // 패스워드의 만료여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // 만료 아님 Non
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
