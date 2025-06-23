4d42ad481a2c07168e70432be272cf94559970f9
JH <yangjae0211@naver.com>
2025-06-24 00:18:31 +0900

    DTO에 toEntitiy 추가

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/SubjectRequest.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/SubjectRequest.java
@@ -1,13 +1,28 @@
 package com.cloudtone.sugang_backend.dto;
 
+import com.cloudtone.sugang_backend.domain.Subject;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor  // 기본 생성자 추가
+@AllArgsConstructor // 모든 필드 값을 파라미터 값으로 받음
 public class SubjectRequest {
     private String subjectName;
     private String subjectCode;
     private String professor;
     private Integer grade;
+
+    // Article 엔티티 생성
+    public Subject toEntity(){
+        return Subject.builder()
+                .subjectName(subjectName)
+                .subjectCode(subjectCode)
+                .professor(professor)
+                .grade(grade)
+                .build();
+    }
 }
--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/SubjectResponse.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/SubjectResponse.java
@@ -1,5 +1,6 @@
 package com.cloudtone.sugang_backend.dto;
 
+import com.cloudtone.sugang_backend.domain.Subject;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,4 +12,13 @@ public class SubjectResponse {
     private String subjectCode;
     private String professor;
     private Integer grade;
+
+    public Subject toEntity(){
+        return Subject.builder()
+                .subjectName(subjectName)
+                .subjectCode(subjectCode)
+                .professor(professor)
+                .grade(grade)
+                .build();
+    }
 }
