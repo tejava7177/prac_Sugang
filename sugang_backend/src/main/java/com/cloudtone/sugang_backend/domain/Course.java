a93e85f93d85b3b12f16b521f585d47eb0080314
JH <yangjae0211@naver.com>
2025-06-24 00:18:31 +0900

    DTO에 toEntitiy 추가

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/domain/Course.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/domain/Course.java
@@ -21,12 +21,13 @@ public class Course {
     // ManyToOne        : 다대일 관계 + 외래키 자동 연결
     // optional = false :  NULL 불가
 
+    /*
+    @ManyToOne(optional = false)
+    private User user;
 
-    @Builder
-    public Course(User user, Subject subject) {
-        this.user = user;
-        this.subject = subject;
-    }
+    @ManyToOne(optional = false)
+    private Subject subject;
+    */
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "user_id")     // ✅ 이거 추가
@@ -35,4 +36,48 @@ public class Course {
     @ManyToOne(optional = false)
     @JoinColumn(name = "subject_id")  // ✅ 이거 꼭 추가!
     private Subject subject;
+
+    @Builder
+    public Course(User user, Subject subject) {
+        this.user = user;
+        this.subject = subject;
+    }
 }
+
+
+/*
+package com.cloudtone.sugang_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "subject_id"})
+})
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // ManyToOne        : 다대일 관계 + 외래키 자동 연결
+    // optional = false :  NULL 불가
+    @ManyToOne(optional = false)
+    private User user;
+
+    @ManyToOne(optional = false)
+    private Subject subject;
+
+    @Builder
+    public Course(User user, Subject subject) {
+        this.user = user;
+        this.subject = subject;
+    }
+}
+ */
--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/AddUserRequest.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/dto/AddUserRequest.java
@@ -1,5 +1,6 @@
 package com.cloudtone.sugang_backend.dto;
 
+import com.cloudtone.sugang_backend.domain.User;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +10,12 @@ public class AddUserRequest { // 학생 정보를 담고 있는 객체
     private String studentNumber;
     private String password;
     private String name;
+
+    public User toEntity(){
+        return User.builder()
+                .studentNumber(studentNumber)
+                .password(password)
+                .name(name)
+                .build();
+    }
 }
