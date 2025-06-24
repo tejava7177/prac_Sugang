44e403388630904f8094d7ed7ef666a2f828a9e8
Sim-JuHeun <juheun9912@gmail.com>
2025-06-24 13:52:11 +0900

    [fix] CourseRepository.java <, Long> 코드 수정

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/repository/CourseRepository.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/repository/CourseRepository.java
@@ -3,6 +3,6 @@ package com.cloudtone.sugang_backend.repository;
 import com.cloudtone.sugang_backend.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course, > {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
