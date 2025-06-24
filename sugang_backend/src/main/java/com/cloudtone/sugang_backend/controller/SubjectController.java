a47c0b60ffd23c5b59a86265111afe7be7868f07
SungYeon <yousung1018@naver.com>
2025-06-25 00:07:23 +0900

    SubjectController

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/SubjectController.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/SubjectController.java
@@ -1,4 +1,38 @@
 package com.cloudtone.sugang_backend.controller;
 
+import com.cloudtone.sugang_backend.domain.Subject;
+import com.cloudtone.sugang_backend.dto.SubjectRequest;
+import com.cloudtone.sugang_backend.dto.SubjectResponse;
+import com.cloudtone.sugang_backend.service.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/subjects")
 public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    // 과목 등록
+    @PostMapping
+    public ResponseEntity<Subject> save(@RequestBody SubjectRequest request) {
+        Subject saved = subjectService.save(request);
+        return ResponseEntity.ok(saved);
+    }
+
+    // 전체 과목 조회
+    @GetMapping
+    public ResponseEntity<List<Subject>> findAll() {
+        return ResponseEntity.ok(subjectService.findAll());
+    }
+
+    // 단일 과목 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<SubjectResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(subjectService.findById(id));
+    }
 }
