2cef84672b1eeb1ce5fadd93142c688442dd1a2a
SungYeon <yousung1018@naver.com>
2025-06-25 01:05:01 +0900

    CourseController

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/CourseController.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/CourseController.java
@@ -1,4 +1,34 @@
 package com.cloudtone.sugang_backend.controller;
 
+import com.cloudtone.sugang_backend.dto.CourseRequest;
+import com.cloudtone.sugang_backend.dto.CourseResponse;
+import com.cloudtone.sugang_backend.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/courses")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Long> applyCourse(@RequestBody CourseRequest request) {
+        return ResponseEntity.ok(courseService.applyCourse(request));
+    }
+
+    @DeleteMapping("/{courseId}")
+    public ResponseEntity<Void> cancelCourse(@PathVariable Long courseId) {
+        courseService.cancelCourse(courseId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<CourseResponse>> getMyCourses(@PathVariable Long userId) {
+        return ResponseEntity.ok(courseService.getMyCourses(userId));
+    }
 }
