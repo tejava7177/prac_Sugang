078c703bd0dac0ba5f58e3d00f6b4630b26b5f19
JH <yangjae0211@naver.com>
2025-06-25 22:36:40 +0900

    CourseController 주석 추가

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/CourseController.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/controller/CourseController.java
@@ -16,17 +16,20 @@ public class CourseController {
 
     private final CourseService courseService;
 
+    // 강의 등록
     @PostMapping
     public ResponseEntity<Long> applyCourse(@RequestBody CourseRequest request) {
         return ResponseEntity.ok(courseService.applyCourse(request));
     }
 
+    // 수강 신청 내역 삭제
     @DeleteMapping("/{courseId}")
     public ResponseEntity<Void> cancelCourse(@PathVariable Long courseId) {
         courseService.cancelCourse(courseId);
         return ResponseEntity.ok().build();
     }
 
+    // 단일 사용자의 수강신청 내역 조회
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<CourseResponse>> getMyCourses(@PathVariable Long userId) {
         return ResponseEntity.ok(courseService.getMyCourses(userId));
