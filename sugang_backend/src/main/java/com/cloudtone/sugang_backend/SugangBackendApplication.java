174caecf0563f35d238fc7f8843757f4a08cf78b
JH <yangjae0211@naver.com>
2025-06-27 21:16:16 +0900

    충돌

--- sugang_backend/src/main/java/com/cloudtone/sugang_backend/SugangBackendApplication.java
+++ sugang_backend/src/main/java/com/cloudtone/sugang_backend/SugangBackendApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SugangBackendApplication {
+public class SugangbackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SugangBackendApplication.class, args);
     }
+    // aaaaaaaaaaaaaaaa
 
 }
